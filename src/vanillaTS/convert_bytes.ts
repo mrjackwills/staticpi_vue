395e8e4d36cf6bbb1d444920d35dbff7ff005df0
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-10-20 01:52:11 +0100

    fix: convert bytes fix

--- src/vanillaTS/convert_bytes.ts
+++ src/vanillaTS/convert_bytes.ts
@@ -6,6 +6,6 @@ export const convert_bytes = (x: number|string): TConvertBytes => {
 	const gb = i / 1000 / 1000 / 1000;
 	const mb = i / 1000 / 1000;
 	return gb >= 1 ? { total: `${Number(i/1000/1000/1000).toFixed(2)}`, unit: `GB` } : mb >= 100 ?
-		{ total: `${Math.trunc(i/1000/1000)}`, unit: `MB` }: mb >= 1 ?
-			{ total: `${Number(i/1000/1000).toFixed(2)}`, unit: `MB` } : { total: `${Math.trunc(i/1000)}`, unit: `kB` };
+		{ total: `${Number(i/1000/1000).toFixed(2)}`, unit: `MB` }: mb >= 1 ?
+			{ total: `${Number(i/1000/1000).toFixed(2)}`, unit: `MB` } : { total: `${Number(i/1000).toFixed(2)}`, unit: `kB` };
 };
